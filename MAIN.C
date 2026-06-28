8cc489317ed372272b05d967862d3273832ac6d3
Alexandre Mamede Patrocinio <133179035+Alexandre-mamede@users.noreply.github.com>
2026-06-28 17:38:37 -0300

    Refactor product saving and menu structure

--- MAIN.C
+++ MAIN.C
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
-#include <struct.h>
+#include "struct.h"
+#include "deposito.h"
+#include "produto.h"
 
 void salvar_produto(produto produtos[], int total){
     
@@ -13,10 +15,10 @@ void salvar_produto(produto produtos[], int total){
     }
     
     for(int i = 0; i < total; i++){
-        fprintf(fp,"%d;%d;%.2f\n",
-                produtos[i].nome,
-                produtos[i].quantidade_itens,
-                produtos[i].valor_Unitario);
+       fprintf(fp,"%s;%d;%.2f\n",
+        produtos[i].nome,
+        produtos[i].quantidade_itens,
+        produtos[i].valor_unitario);
     }
     
     fclose(fp);
@@ -37,25 +39,86 @@ int opcao;
     
     switch (opcao){
         
-        case 1:
-            printf("\n===Menu Depósitos===\n\n");
-            printf("1. Cadastrar novo depósito\n");
-            printf("2. Consultar informações sobre um depósito\n");
-            printf("3. Consultar informações sobre todos os depósitos\n");
-            printf("4. Atualizar dados de um depósito\n");
-            printf("5. Remover um depósito\n");
-            printf("6. Voltar ao menu principal\n");
-            break;
-           
-        case 2:
-            printf("\n===Menu Produtos===\n\n");
-            printf("1. Cadastrar novo produto\n");
-            printf("2. Consultar produto\n");
-            printf("3. Atualizar dados de um produto\n");
-            printf("4. Remover produto\n");
-            printf("5. Transferir produto para outro depósito\n");
-            printf("6. Voltar ao menu principal\n");
-            break;
+       case 1:
+
+    int opDeposito;
+
+    do{
+
+        printf("\n=== MENU DEPOSITO ===\n");
+        printf("1-Cadastrar\n");
+        printf("2-Consultar\n");
+        printf("3-Consultar todos\n");
+        printf("4-Atualizar\n");
+        printf("5-Voltar\n");
+
+        scanf("%d",&opDeposito);
+
+        switch(opDeposito){
+
+            case 1:
+
+                totalDepositos++;
+                Cadastrar_Novo_Deposito(depositos,totalDepositos);
+                break;
+
+            case 2:
+
+                Consultar_Inf_Depositos(depositos,totalDepositos);
+                break;
+
+            case 3:
+
+                Consultar_Todas_Inf_Depositos(depositos,totalDepositos);
+                break;
+
+            case 4:
+
+                Atualizar_Dados_Deposito(depositos,totalDepositos);
+                break;
+
+        }
+
+    }while(opDeposito!=5);
+
+    break;
+       case 2:
+
+    int opProduto;
+
+    do{
+
+        printf("\n=== MENU PRODUTO ===\n");
+        printf("1-Cadastrar\n");
+        printf("2-Consultar\n");
+        printf("3-Atualizar\n");
+        printf("4-Voltar\n");
+
+        scanf("%d",&opProduto);
+
+        switch(opProduto){
+
+            case 1:
+
+                cadastrar_produto(produtos[totalProdutos],depositos[0]);
+                totalProdutos++;
+                break;
+
+            case 2:
+
+                consultar_produto(produtos[0],depositos[0]);
+                break;
+
+            case 3:
+
+                atualiza_produto();
+                break;
+
+        }
+
+    }while(opProduto!=4);
+
+    break;
        case 3:
             salvar_produto(produtos,total);
             printf("saindo......");
