800b82a423c91119728c7f757654ea6af77f8cfd
Alexandre Mamede Patrocinio <133179035+Alexandre-mamede@users.noreply.github.com>
2026-06-28 17:34:48 -0300

    Add produto.h header file with product functions

---
+++ produto.h
@@ -0,0 +1,10 @@
+#ifndef PRODUTO_H
+#define PRODUTO_H
+
+#include "struct.h"
+
+char cadastrar_produto(produto produtos, Deposito depositos);
+int consultar_produto(produto produtos, Deposito depositos);
+void atualiza_produto();
+
+#endif
