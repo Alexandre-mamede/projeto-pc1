eaf0fd4521f2a08a4c7a1748f8fd141e8e403e94
Alexandre Mamede Patrocinio <133179035+Alexandre-mamede@users.noreply.github.com>
2026-06-28 17:34:24 -0300

    Create deposito.h

---
+++ deposito.h
@@ -0,0 +1,11 @@
+#ifndef DEPOSITO_H
+#define DEPOSITO_H
+
+#include "struct.h"
+
+void Cadastrar_Novo_Deposito(Deposito depositos[], int contador);
+void Consultar_Inf_Depositos(Deposito depositos[], int contador);
+void Consultar_Todas_Inf_Depositos(Deposito depositos[], int contador);
+void Atualizar_Dados_Deposito(Deposito depositos[], int contador);
+
+#endif
