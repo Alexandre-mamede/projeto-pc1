2a1df8d58c8deda832487b4c06637f7ce0a2a556
Miguel Moura Mendes <miguelmoura247@gmail.com>
2026-06-29 22:49:48 -0300

    Update produto.h

--- produto.h
+++ produto.h
@@ -1,10 +1,32 @@
 #ifndef PRODUTO_H
 #define PRODUTO_H
 
-#include "struct.h"
+// forward declaration (evita dependência circular)
+struct Deposito;
 
-char cadastrar_produto(produto produtos, Deposito depositos);
-int consultar_produto(produto produtos, Deposito depositos);
-void atualiza_produto();
+typedef struct {
+    char codigo_barras[13];
+    char nome_produto[101];
+    int tipo_produto;
+    int perecivel;
+    char data_fabricacao[11];
+    float volume_unidade;
+    int quantidade_itens;
+    float valor_unitario;
+    char data_validade[11];
+} produto;
+
+char cadastrar_produto(produto *p, struct Deposito depositos);
+
+int consultar_produto(void);
+void atualiza_produto(void);
+void remover_produto(void);
+void transferir_produto(void);
+
+int validar_codigo(char codigo[]);
+int validar_cep(const char *cep);
+int verificar_data(produto prod);
+
+int alocar_produto_no_deposito_por_indice(produto novo_produto, const char *cep_busca);
 
 #endif
