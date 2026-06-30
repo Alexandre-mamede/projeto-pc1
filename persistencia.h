c04bedb7330f19646c3be14a38f3d74d18190fbd
Alexandre Mamede Patrocinio <133179035+Alexandre-mamede@users.noreply.github.com>
2026-06-29 22:13:30 -0300

    Create persistencia.h

---
+++ persistencia.h
@@ -0,0 +1,10 @@
+#IFNDEF PERSISTENCIA_H
+#DEFINE PERSISTENCIA_H
+
+#include "struct.h"
+
+int carregarDados(Deposito depositos[]);
+
+int salvarDados(Deposito depositos[], int total_depositos);
+
+#endif
